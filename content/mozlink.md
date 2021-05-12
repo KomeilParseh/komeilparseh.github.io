99e3608f0b30b3a00666437aca13266117344c40
komeilparseh <ahmdparsh129@gmail.com>
2021-05-12 15:10:44 +0430

    Migrate from handwriting HTML to MD

---
+++ content/mozlink.md
@@ -0,0 +1,42 @@
+Title:Mozlink!
+Date:2021-5-4-21:00
+Modified:2021-5-4-21:00
+Category:programing
+Tags:tecnology,programin,python,mozlink
+Slug:Mozlink
+Authors:komeilParseh
+Summary:A program to build shortLinks
+
+# mozlink
+
+![logo](https://raw.githubusercontent.com/KomeilParseh/mozlink/main/app/static/logo.png)
+
+A program to build **shortLinks**
+
+[mozlink-komeilparseh.fandogh.cloud](https://mozlink-komeilparseh.fandogh.cloud/) Temporary (perhaps permanent!) service link.
+
+Note: Sometimes links may **not work** due to site updates.
+
+## How to run
+
+1. Install python3, pip3, virtualenv in your system.
+2. Clone the project
+
+`git clone https://github.com/KomeilParseh/mozlink.git && cd mozlink`
+
+3. in the app folder, rename the `config.py.sample` to `config.py` and do proper changes.
+4. Create a virtualenv named venv using
+
+`virtualenv -p python3 venv`
+
+5. Connect to virtualenv using
+
+`source venv/bin/activate`
+
+6. From the project folder, install packages using
+
+`pip install -r requirements.txt`
+
+7. Now environment is ready. Run it by
+
+`python app/main.py`
---
+++ pelicanconf.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Komeil Parseh'
+SITENAME = 'Komeil Parseh'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Tehran'
+
+DEFAULT_LANG = 'en'
+
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('twitter', 'http://twitter.com/@komeilparseh'),
+          ('github', 'https://github.com/komeilparseh'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# Theme Setting
+THEME = 'blue-penguin'
+# all the following settings are *optional*
+
+# HTML metadata
+SITEDESCRIPTION = ''
+
+# all defaults to True.
+DISPLAY_HEADER = True
+DISPLAY_FOOTER = True
+DISPLAY_HOME = True
+DISPLAY_MENU = True
+
+# provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
+TAGS_URL = 'tags'
+TAGS_SAVE_AS = 'tags/index.html'
+CATEGORIES_URL = 'categories'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+
+
+# use those if you want pelican standard pages to appear in your menu
+MENU_INTERNAL_PAGES = (
+    ('Tags', TAGS_URL, TAGS_SAVE_AS),
+    ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
+)
+# additional menu items
+MENUITEMS = (
+    ('GitHub', 'https://github.com/komeilparseh'),
+    ('Persian Blog', 'https://virgool.io/@parseh'),
+)
